6fb52d5dd8bc87433dc88d99cccf23d315dd7809
aeioiie <144753583+aeioiie@users.noreply.github.com>
2024-05-16 16:05:08 +0900

    [D4] Title: [S/W 문제해결 기본] 10일차 - Contact, Time: 140 ms, Memory: 48,448 KB -BaekjoonHub

--- SWEA/D4/1238. ［S／W 문제해결 기본］ 10일차 － Contact/［S／W 문제해결 기본］ 10일차 － Contact.py
+++ SWEA/D4/1238. ［S／W 문제해결 기본］ 10일차 － Contact/［S／W 문제해결 기본］ 10일차 － Contact.py
@@ -1,30 +1,38 @@
 from collections import deque
 
-def solution(graph, start):
-    visited = list()
-    queue = deque()
-    queue.append((start,0))
-    visited.append(start)
-    howlen=list()
+def bfs(start):
+    count = 1
+    queue = deque([[start, count]])
+    visited[start][0] = True
+
     while queue:
-        node, count = queue.popleft()
-        if node in graph:
-            for i in graph[node]:
-                if i not in visited:
-                    visited.append(i)
-                    howlen.append((i,count+1))
-                    queue.append((i,count+1))
-    howlen.sort(key = lambda x : (-x[1],-x[0]))
-    return howlen[0][0]
+        v, count = queue.popleft()
 
-for test_case in range(1,11):
-    N, start = map(int,input().split())
-    numlist = list(map(int,input().split()))
-    graph={}
-    for i in range(0,N,2):
-        if numlist[i] not in graph:
-            graph[numlist[i]] = [numlist[i+1]]
-        else:
-            graph[numlist[i]].append(numlist[i+1])
+        for i in graph[v]:
+            if not visited[i][0]:
+                visited[i][0] = True
+                visited[i][1] = count + 1
+                queue.append([i, count + 1])
 
-    print('#{} {}'.format(test_case, solution(graph,start)))
+for tc in range(1, 10 + 1):
+    N, v = map(int, input().split())
+
+    graph = [[] for _ in range(100 + 1)]  # 그래프 저장
+    visited = [[False, 0] for i in range(100 + 1)]  # 방문여부, 방문차례
+    info = list(map(int, input().split()))
+    for i in range(0, N, 2):  # 그래프 정보를 저장
+        from_, to_ = info[i], info[i + 1]
+        graph[from_].append(to_)
+
+    bfs(v)  # bfs를 돈다
+
+    max_ = 0  # 전화를 받은 순서중 가장 큰값을 저장
+    result = 0  # 큰 값중에서 가장 마지막 사람을 저장
+
+    # 전화를 받은 순서 중 가장 큰 값과 그에 해당하는 가장 큰 번호를 찾는다.
+    for i in range(1, 100 + 1):
+        if max_ <= visited[i][1]:
+            max_ = visited[i][1]
+            result = i
+
+    print(f'#{tc} {result}')

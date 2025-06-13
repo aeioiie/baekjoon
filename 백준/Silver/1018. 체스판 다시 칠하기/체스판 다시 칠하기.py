4bef36ba515f48acbda7faceb69836447bf6d15c
Saeum Jung <144753583+aeioiie@users.noreply.github.com>
2025-06-14 02:39:06 +0900

    [Silver IV] Title: 체스판 다시 칠하기, Time: 88 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.py
@@ -0,0 +1,42 @@
+N, M = map(int, input().split())
+inputarray = []
+
+for i in range(N):
+    inputarray.append([])
+    horizontal = list(input())
+    inputarray[i] = horizontal
+
+# print(inputarray)
+
+WBarray = [[], [], [], [], [], [], [], []]
+BWarray = [[], [], [], [], [], [], [], []]
+
+for i in range(8):
+    for j in range(8):
+        if (i+j) % 2 == 0:
+            WBarray[i].append('W')
+            BWarray[i].append('B')
+        else:
+            WBarray[i].append('B')
+            BWarray[i].append('W')
+
+# print(WBarray)
+# print(BWarray)
+
+min_diff = 999999
+
+for i in range(N-7):
+    for j in range(M-7):
+        WBdiff = 0
+        BWdiff = 0
+
+        for k in range(8):
+            for l in range(8):
+                if inputarray[i+k][j+l] != WBarray[k][l]:
+                    WBdiff += 1
+                if inputarray[i+k][j+l] != BWarray[k][l]:
+                    BWdiff += 1
+        
+        min_diff = min(min_diff, WBdiff, BWdiff)
+
+print(min_diff)

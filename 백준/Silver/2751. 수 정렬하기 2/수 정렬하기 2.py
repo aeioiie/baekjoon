8c9e4656e1cd2bc8ab52c6ce568bea14d25e7f70
Saeum Jung <144753583+aeioiie@users.noreply.github.com>
2025-06-16 00:16:00 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 1432 ms, Memory: 78268 KB -BaekjoonHub

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.py
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.py
@@ -1,46 +1,14 @@
 import sys
 input = sys.stdin.readline
 
-N = int(input().strip())
-l = []
+N = int(input())
+num = []
 
-# 한 줄씩 입력을 받아 리스트에 추가
-for _ in range(N):
-    l.append(int(input().strip()))
+for i in range(N):
+    x = int(input())
+    num.append(x)
 
-def merge_sort(m):
-    if len(m) <= 1:
-        return m
+num.sort()
 
-    mid = len(m) // 2
-    left = m[:mid]
-    right = m[mid:]
-
-    left = merge_sort(left)
-    right = merge_sort(right)
-
-    return merge(left, right)
-
-def merge(left, right):
-    result = []
-    i = j = 0
-
-    # 각 리스트의 요소를 하나씩 비교하여 결과 리스트에 추가
-    while i < len(left) and j < len(right):
-        if left[i] <= right[j]:
-            result.append(left[i])
-            i += 1
-        else:
-            result.append(right[j])
-            j += 1
-
-    # 남아 있는 요소들을 결과에 추가
-    result.extend(left[i:])
-    result.extend(right[j:])
-    return result
-
-# 정렬된 리스트 얻기
-sorted_l = merge_sort(l)
-
-# 결과 출력
-sys.stdout.write("\n".join(map(str, sorted_l)) + "\n")
+for i in range(N):
+    print(num[i])

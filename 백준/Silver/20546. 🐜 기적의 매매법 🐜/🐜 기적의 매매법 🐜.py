f66ffb39b321935fda1c1de1be85edfc6eb86f2b
Saeum Jung <144753583+aeioiie@users.noreply.github.com>
2025-07-15 19:17:31 +0900

    [Silver V] Title: 🐜 기적의 매매법 🐜, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/20546. 🐜 기적의 매매법 🐜/🐜 기적의 매매법 🐜.py
+++ 백준/Silver/20546. 🐜 기적의 매매법 🐜/🐜 기적의 매매법 🐜.py
@@ -20,10 +20,9 @@ for i in range(len(stock_prices)):
             SM_money += stock_prices[i] * SM_buy
             SM_buy = 0
         elif stock_prices[i-3] > stock_prices[i-2] and stock_prices[i-2] > stock_prices[i-1] and stock_prices[i-1] > stock_prices[i]: # i+1번째 날에는 주식이 상승할 것 -> 전량 매수
-            if stock_prices[i] <= SM_money:
-                SM_buy_now = SM_money // stock_prices[i]
-                SM_buy += SM_buy_now
-                SM_money -= SM_buy_now * stock_prices[i]
+            SM_buy_now = SM_money // stock_prices[i]
+            SM_buy += SM_buy_now
+            SM_money -= SM_buy_now * stock_prices[i]
             # 예를들어서 현재 현금이 20, 주가가 33, 보유 주식 수가 2라면
             # 전량 매도 시 현금은 20 + 33 x 2 = 86, 보유 주식 수는 0
             # 주가 x 보유 주식 수 + 현금

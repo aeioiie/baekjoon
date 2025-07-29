f3028bacf7f1f9d798f1a415c8a1960492c33daf
Saeum Jung <144753583+aeioiie@users.noreply.github.com>
2025-07-29 23:21:40 +0900

    [Silver III] Title: 1로 만들기, Time: 244 ms, Memory: 40224 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.py
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.py
@@ -1,12 +1,15 @@
 N = int(input())
 
-dp = [0] * 10000000
+# 10 -> 9 -> 3 -> 1
+def cal(n):
+    dp = [0] * (n+1)
+    for i in range(2, n+1):
+        dp[i] = dp[i-1] + 1 # -1
+        if i % 2 == 0: # // 2
+            dp[i] = min(dp[i], dp[i//2] + 1)
+        if i % 3 == 0: # // 3
+            dp[i] = min(dp[i], dp[i//3] + 1)
+    
+    return dp[n]
 
-for i in range(2, N+1):
-    dp[i] = dp[i-1] + 1
-    if i % 2 == 0:
-        dp[i] = min(dp[i], dp[i//2]+1)
-    if i % 3 == 0:
-        dp[i] = min(dp[i], dp[i//3]+1)
-
-print(dp[N])
+print(cal(N))

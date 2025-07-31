f5c4bba72613c698790c77f95a19e6d335823553
Saeum Jung <144753583+aeioiie@users.noreply.github.com>
2025-08-01 01:52:30 +0900

    [Gold V] Title: 퇴사 2, Time: 2344 ms, Memory: 170024 KB -BaekjoonHub

---
+++ 백준/Gold/15486. 퇴사 2/퇴사 2.py
@@ -0,0 +1,21 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+time = []
+profit = []
+
+for i in range(N):
+    T, P = map(int, input().split())
+    time.append(T)
+    profit.append(P)
+
+dp = [0] * (N+1) # dp[i] = i일까지 얻을 수 있는 최대 이익
+
+for i in range(N):
+    dp[i+1] = max(dp[i+1], dp[i]) # 상담을 하지 않는 경우
+
+    if i + time[i] <= N:  # 상담을 할 수 있는 경우
+        dp[i + time[i]] = max(dp[i + time[i]], dp[i] + profit[i])
+
+print(dp[N])
